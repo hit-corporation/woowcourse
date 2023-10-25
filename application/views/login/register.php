1df7ebdc1128798726457a0e45d72965fedb72d9
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-10-25 14:27:34 +0700

    add input email pada UI register new user

--- application/views/login/register.php
+++ application/views/login/register.php
@@ -54,6 +54,12 @@
                                         placeholder="Email Address">
 										<?php echo form_error('email'); ?>
                                 </div>
+                                <div class="form-group">
+                                    <select type="type" class="form-control" name="type" id="type">
+										<option value="2">Member</option>
+										<option value="3">instructor</option>
+									</select>
+                                </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user"
