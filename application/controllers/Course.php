6ec58f6533ec2b0cf7f739f0047f1092560ceea0
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-11-09 10:10:38 +0700

    create function get_all data course pada controller course

--- application/controllers/Course.php
+++ application/controllers/Course.php
@@ -4,6 +4,7 @@ class Course extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
+		$this->load->model(['topics_model']);
     }
 
     /**
@@ -12,6 +13,23 @@ class Course extends MY_Controller {
      * @return void
      */
     public function index(): void {
-        echo $this->template->render('index');
+		$data['topics'] = $this->topics_model->get_all();
+        echo $this->template->render('index', $data);
     }
+
+	public function get_all(){
+		$page 			= isset($_GET['page']) ? (int)$_GET['page'] : 1;
+		$limit 			= isset($_GET['limit']) ? (int)$_GET['limit'] : 3;
+		$filter['title']= isset($_GET['title']) ? $_GET['title'] : '';
+
+		$page = ($page - 1) * $limit;
+
+		$data['data'] 			= $this->topics_model->get_all($filter, $limit, $page);
+		$data['total_records'] 	= $this->topics_model->get_total($filter);
+		$data['total_pages'] 	= ceil($data['total_records'] / $limit);
+
+		// create json header	
+		header('Content-Type: application/json');
+		echo json_encode($data);
+	}
 }
