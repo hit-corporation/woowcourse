0e7bcb9d921637579225899bc037a10b7d7e0add
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-11-10 17:21:50 +0700

    create js event menu category di klik

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,21 @@
 const BASE_URL = document.querySelector('base').href;
-console.log(BASE_URL);
+
+let items = document.querySelectorAll('.nav-item[data]');
+items.forEach((value, key) => {
+	value.addEventListener('click', function(e){
+		//let valueOnHover = value.attributes.data.value;
+
+		// [...CATEGORIES].forEach((val) => {
+		// 	if(valueOnHover == val.id){
+		// 		console.log(val.child);
+		// 	}
+		// });
+		e.preventDefault();
+
+		if(value.nextElementSibling.style.display === 'none'){
+			value.nextElementSibling.style.display = 'block';
+		}else{
+			value.nextElementSibling.style.display = 'none';
+		}
+	});
+});
