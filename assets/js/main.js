ae96b317ba3986f0c0a9832b79895d062e2eaddf
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-11-13 10:22:46 +0700

    create local storage category pada main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,6 @@ const BASE_URL = document.querySelector('base').href;
 let items = document.querySelectorAll('.nav-item[data]');
 items.forEach((value, key) => {
 	value.addEventListener('click', function(e){
-		//let valueOnHover = value.attributes.data.value;
-
-		// [...CATEGORIES].forEach((val) => {
-		// 	if(valueOnHover == val.id){
-		// 		console.log(val.child);
-		// 	}
-		// });
 		e.preventDefault();
 
 		if(value.nextElementSibling.style.display === 'none'){
@@ -19,3 +12,7 @@ items.forEach((value, key) => {
 		}
 	});
 });
+
+function categoryClick(id){
+	localStorage.setItem('category', id);
+}
