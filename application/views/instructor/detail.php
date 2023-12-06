fe7dc94edef5ee14f797c2abbbb409e8d8d50e7b
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-12-06 10:37:15 +0700

    remark teacher js pada UI detail.php

--- application/views/instructor/detail.php
+++ application/views/instructor/detail.php
@@ -243,5 +243,5 @@ $this->layout('layouts::main_template', ['title' => 'Instructor']) ?>
 
 <?php $this->start('js') ?>
 	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-	<script src="assets/js/teacher.js" async defer></script>
+	<!-- <script src="assets/js/teacher.js" async defer></script> -->
 <?php $this->end() ?>
