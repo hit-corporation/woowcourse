bd4d507c057e5730b8ec9b2fee1fa4180f9e0fff
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-12-11 17:33:12 +0700

    db

--- woowcourse.sql
+++ woowcourse.sql
@@ -102,11 +102,13 @@ CREATE TABLE public.courses (
     course_code character varying(245) NOT NULL,
     course_title character varying(245) NOT NULL,
     course_img text,
-    description character varying(255),
+    description text,
     instructor_id bigint NOT NULL,
     category_id bigint NOT NULL,
     created_at timestamp(0) without time zone,
-    updated_at timestamp(0) without time zone
+    updated_at timestamp(0) without time zone,
+    rating real,
+    price integer
 );
 
 
@@ -145,7 +147,9 @@ CREATE TABLE public.instructors (
     last_name character varying(200),
     email character varying(120),
     phone character varying(20),
-    photo text
+    photo text,
+    about text,
+    address character varying(255)
 );
 
 
@@ -185,7 +189,8 @@ CREATE TABLE public.members (
     email character varying(200),
     photo text,
     phone character varying(20),
-    last_login_date timestamp(0) without time zone NOT NULL
+    last_login_date timestamp(0) without time zone NOT NULL,
+    as_instructor boolean DEFAULT false NOT NULL
 );
 
 
@@ -375,7 +380,8 @@ CREATE TABLE public.topics (
     instructor_id integer NOT NULL,
     content_file text,
     description text,
-    price double precision
+    price double precision,
+    rating real
 );
 
 
@@ -578,6 +584,63 @@ COPY public.actionlogs (id, created_at, updated_at, "user", ipadd, logtime, logd
 --
 
 COPY public.categories (id, created_at, updated_at, category_name, parent_category) FROM stdin;
+1	2023-11-10 10:22:00	\N	All	0
+2	2023-11-10 10:22:00	\N	Pengembangan	1
+3	2023-11-10 10:22:00	\N	Pengembangan Web	2
+4	2023-11-10 10:22:00	\N	Ilmu Data	2
+5	2023-11-10 10:22:00	\N	Pengembangan Seluler	2
+6	2023-11-10 10:22:00	\N	Bahasa Pemrograman	2
+7	2023-11-10 10:22:00	\N	Pengembangan Game	2
+8	2023-11-10 10:22:00	\N	Desain dan Pengembangan Database	2
+9	2023-11-10 10:22:00	\N	Pengujian Perangkat Lunak	2
+10	2023-11-10 10:22:00	\N	Rekayasa Perangkat Lunak	2
+11	2023-11-10 10:22:00	\N	Alat Pengembangan Perangkat Lunak	2
+12	2023-11-10 10:22:00	\N	Pengembangan Tanpa Kode	2
+13	2023-11-10 10:22:00	\N	Bisnis	1
+14	2023-11-10 10:22:00	\N	Kewirausahaan	13
+15	2023-11-10 10:22:00	\N	Komunikasi	13
+16	2023-11-10 10:22:00	\N	Manajemen	13
+17	2023-11-10 10:22:00	\N	Penjualan	13
+18	2023-11-10 10:22:00	\N	Strategi Bisnis	13
+19	2023-11-10 10:22:00	\N	Operasi	13
+20	2023-11-10 10:22:00	\N	Manajemen Proyek	13
+21	2023-11-10 10:22:00	\N	Hukum Bisnis	13
+22	2023-11-10 10:22:00	\N	Analisis dan Kecerdasan Bisnis	13
+23	2023-11-10 10:22:00	\N	Sumber Daya Manusia	13
+24	2023-11-10 10:22:00	\N	Industri	13
+25	2023-11-10 10:22:00	\N	Ecomerce	13
+26	2023-11-10 10:22:00	\N	Media	13
+27	2023-11-10 10:22:00	\N	Real Estate	13
+28	2023-11-10 10:22:00	\N	Bisnis Lainnya	13
+29	2023-11-10 10:22:00	\N	Keuangan dan Akuntansi	1
+30	2023-11-10 10:22:00	\N	Akuntansi dan Laporan Keuangan	29
+31	2023-11-10 10:22:00	\N	Kepatuhan	29
+32	2023-11-10 10:22:00	\N	Cryptocurrency dan Blockchain	29
+33	2023-11-10 10:22:00	\N	Ekonomi	29
+34	2023-11-10 10:22:00	\N	Keuangan	29
+35	2023-11-10 10:22:00	\N	Sertifikasi dan Persiapan Ujian Keuangan	29
+36	2023-11-10 10:22:00	\N	Pemodelan dan Analisis Keuangan	29
+37	2023-11-10 10:22:00	\N	Investasi dan Perdagangan	29
+38	2023-11-10 10:22:00	\N	Alat Manajemen Keuangan	29
+39	2023-11-10 10:22:00	\N	Pajak	29
+40	2023-11-10 10:22:00	\N	Keuangan dan Akuntanti Lainnya	29
+41	2023-11-10 10:22:00	\N	TI dan Perangkat Lunak	1
+42	2023-11-10 10:22:00	\N	Serifikasi TI	41
+43	2023-11-10 10:22:00	\N	Jaringan dan Keamanan	41
+44	2023-11-10 10:22:00	\N	Perangkat Keras	41
+45	2023-11-10 10:22:00	\N	Sistem Operasi & Server	41
+46	2023-11-10 10:22:00	\N	TI Perangkat Lunak & Lainnya	41
+47	2023-11-10 10:22:00	\N	Desain	1
+48	2023-11-10 10:22:00	\N	Desain Web	47
+49	2023-11-10 10:22:00	\N	Alat Desain	47
+50	2023-11-10 10:22:00	\N	Desain Grafis & Ilustrasi	47
+51	2023-11-10 10:22:00	\N	Desain Pengalaman Pengguna	47
+52	2023-11-10 10:22:00	\N	Desain Game	47
+53	2023-11-10 10:22:00	\N	3D & Animasi	47
+54	2023-11-10 10:22:00	\N	Desain Fashion	47
+55	2023-11-10 10:22:00	\N	Desain Arsitektur	47
+56	2023-11-10 10:22:00	\N	Desain Interior	47
+57	2023-11-10 10:22:00	\N	Desain Lainnya	47
 \.
 
 
@@ -585,7 +648,10 @@ COPY public.categories (id, created_at, updated_at, category_name, parent_catego
 -- Data for Name: courses; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.courses (id, course_code, course_title, course_img, description, instructor_id, category_id, created_at, updated_at) FROM stdin;
+COPY public.courses (id, course_code, course_title, course_img, description, instructor_id, category_id, created_at, updated_at, rating, price) FROM stdin;
+3	C0002	Pemrograman PHP Menengah	\N	Pemrograman PHP Menengah	1	3	2023-11-13 08:56:00	\N	3.20000005	150000
+4	C0003	Pemrograman PHP Expert	\N	Pemrograman PHP Expert	1	3	2023-11-13 08:56:00	\N	4.80000019	125000
+2	C0001	Pemrograman PHP Pemula Dari Nol	\N	Selamat datang di Tutorial Belajar PHP untuk pemula..\r\n\r\nBanyak pemula yang bingung dan bertanya:\r\n\r\n“Gimana sih cara membuat web dengan PHP?”\r\n\r\n“Apa saja alat-alat yang diperlukan untuk coding PHP?”\r\n\r\n“Mengapa kita butuh PHP?”\r\n\r\n..dan masih banyak pertanyaan lainnya.\r\n\r\nTentang..\r\n\r\nKita akan mempelajarinya sampai paham.\r\n\r\nMulai dari sejarah awal kemunculan PHP, sampai bisa paham konsep dasar pemrograman PHP.	1	3	2023-11-13 08:56:00	\N	4.5	99000
 \.
 
 
@@ -593,7 +659,9 @@ COPY public.courses (id, course_code, course_title, course_img, description, ins
 -- Data for Name: instructors; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.instructors (id, created_at, updated_at, first_name, last_name, email, phone, photo) FROM stdin;
+COPY public.instructors (id, created_at, updated_at, first_name, last_name, email, phone, photo, about, address) FROM stdin;
+1	2023-11-13 08:55:00	\N	Abduh	Lestaluhu	abduh.lestaluhu@gmail.com	085621249433	3.jpg	Lecturer of Informatics Engineering Dept. at Pasundan University, Bandung - Indonesia.\r\n\r\nCurrently teaching Web Design and Web Programming.\r\n\r\nAlso doing research in Web Technology, Multimedia, Cognitive Science and UI/UX Design.	Jl. Bangka Raya, Kebayoran Baru, Jakarta Selatan, DKI Jakarta
+11	\N	\N	tes 1	tes 1	fauzi.enginer@gmail.com	tes 1	46c9e11c14146b1aec1be924063b0405.jpg	<p>tes 1</p>	tes 1
 \.
 
 
@@ -601,7 +669,14 @@ COPY public.instructors (id, created_at, updated_at, first_name, last_name, emai
 -- Data for Name: members; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.members (id, created_at, updated_at, first_name, last_name, email, photo, phone, last_login_date) FROM stdin;
+COPY public.members (id, created_at, updated_at, first_name, last_name, email, photo, phone, last_login_date, as_instructor) FROM stdin;
+2	\N	\N	Tony	Sucipto	tony.sucipto@gmail.com	\N	\N	2023-11-14 10:56:00	f
+4	\N	\N	Marko	Simic	marko.simic@gmail.com	\N	\N	2023-11-14 10:56:00	f
+3	\N	\N	Riko	Simanjuntak	riko.simanjuntak@gmail.com	\N	\N	2023-11-14 10:56:00	f
+5	\N	\N	Maman	Abdurahman	maman.abdurahman@gmail.com	\N	\N	2023-11-14 10:56:00	f
+6	\N	\N	Andritany	Ardhiyasa	andritany.ardhiyasa@gmail.com	\N	\N	2023-11-14 10:56:00	f
+7	\N	\N	Hansamu	Yama	hansamu.yama@gmail.com	\N	\N	2023-11-14 10:56:00	f
+22	\N	\N	tes 1	tes 1	fauzi.enginer@gmail.com	6162b791c3da45b99b7353240210e6fe.jpeg	tes 1	2023-11-22 15:51:16	t
 \.
 
 
@@ -629,6 +704,7 @@ COPY public.migrations (id, migration, batch) FROM stdin;
 --
 
 COPY public.personal_access_tokens (id, user_no, name, token, abilities, expired_at, last_used_at, created_at, updated_at) FROM stdin;
+1	cb9f5c7d-3878-4769-be7a-b1da74d80986	reg_1700029422	qw2ZEeNWeAvxt7BFoP6U181s	\N	2023-11-15 14:23:42	\N	2023-11-15 13:23:42	2023-11-15 13:23:42
 \.
 
 
@@ -637,6 +713,11 @@ COPY public.personal_access_tokens (id, user_no, name, token, abilities, expired
 --
 
 COPY public.ratings (id, created_at, updated_at, topic_id, member_id, rate) FROM stdin;
+1	\N	\N	1	1	4
+2	\N	\N	1	2	5
+3	\N	\N	1	3	4
+4	\N	\N	2	1	3
+5	\N	\N	2	2	5
 \.
 
 
@@ -645,6 +726,10 @@ COPY public.ratings (id, created_at, updated_at, topic_id, member_id, rate) FROM
 --
 
 COPY public.subscriptions (id, created_at, updated_at, topic_id, member_id, instructor_id, start_date, end_date) FROM stdin;
+3	2023-11-01 00:00:00	\N	2	1	1	2023-11-01 00:00:00	2023-11-30 00:00:00
+4	2023-11-01 00:00:00	\N	3	2	1	2023-11-01 00:00:00	2023-11-30 00:00:00
+1	2023-11-01 00:00:00	\N	4	1	1	2023-11-01 00:00:00	2023-11-30 00:00:00
+2	2023-11-01 00:00:00	\N	4	2	1	2023-11-01 00:00:00	2023-11-30 00:00:00
 \.
 
 
@@ -652,7 +737,7 @@ COPY public.subscriptions (id, created_at, updated_at, topic_id, member_id, inst
 -- Data for Name: topics; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.topics (id, created_at, updated_at, category_id, title, instructor_id, content_file, description, price) FROM stdin;
+COPY public.topics (id, created_at, updated_at, category_id, title, instructor_id, content_file, description, price, rating) FROM stdin;
 \.
 
 
@@ -669,6 +754,8 @@ COPY public.user_level (id, created_at, updated_at, user_level_id, user_level_na
 --
 
 COPY public.users (id, created_at, updated_at, user_code, first_name, last_name, email, password, user_level, last_login, active, photo) FROM stdin;
+1	2023-11-15 13:23:42	2023-11-15 13:23:42	cb9f5c7d-3878-4769-be7a-b1da74d80986	Web	Developer	fauzi.enginer@gmail.com	$2y$10$pieq7t0.374r44BwVz4j0uvVfEO5OIjOIBdY/OctznCwZHsSvZhQy	2	2023-11-16 02:51:42	1	\N
+3	2023-11-15 13:23:42	2023-11-15 13:23:42	cb9f5c7d-3878-4769-be7a-b1da74d80987	\N	\N	abduh.lestaluhu@gmail.com	$2y$10$pieq7t0.374r44BwVz4j0uvVfEO5OIjOIBdY/OctznCwZHsSvZhQy	2	2023-11-15 13:23:42	1	\N
 \.
 
 
@@ -690,21 +777,21 @@ SELECT pg_catalog.setval('public.categories_id_seq', 1, false);
 -- Name: courses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.courses_id_seq', 1, false);
+SELECT pg_catalog.setval('public.courses_id_seq', 4, true);
 
 
 --
 -- Name: instructors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.instructors_id_seq', 1, false);
+SELECT pg_catalog.setval('public.instructors_id_seq', 11, true);
 
 
 --
 -- Name: members_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.members_id_seq', 1, false);
+SELECT pg_catalog.setval('public.members_id_seq', 22, true);
 
 
 --
@@ -718,21 +805,21 @@ SELECT pg_catalog.setval('public.migrations_id_seq', 11, true);
 -- Name: personal_access_tokens_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.personal_access_tokens_id_seq', 1, false);
+SELECT pg_catalog.setval('public.personal_access_tokens_id_seq', 1, true);
 
 
 --
 -- Name: ratings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ratings_id_seq', 1, false);
+SELECT pg_catalog.setval('public.ratings_id_seq', 5, true);
 
 
 --
 -- Name: subscriptions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.subscriptions_id_seq', 1, false);
+SELECT pg_catalog.setval('public.subscriptions_id_seq', 4, true);
 
 
 --
@@ -753,7 +840,7 @@ SELECT pg_catalog.setval('public.user_level_id_seq', 1, false);
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_id_seq', 3, true);
 
 
 --
