ce31144b8281e860d01970d93d0af359f2966ba9
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-12-21 10:23:53 +0700

    fixing detail_course.js

--- assets/js/detail_course.js
+++ assets/js/detail_course.js
@@ -18,7 +18,7 @@ async function addComment(e) {
 
         const tbody = tableComment.tBodies[0];
 
-        const f = await fetch('/rating/store', {
+        const f = await fetch(BASE_URL+'/rating/store', {
             method: 'POST',
             body: new URLSearchParams(obj).toString(),
             headers: {
@@ -27,6 +27,7 @@ async function addComment(e) {
         });
 
         const j = await f.json();
+
         
         if(!f.ok)
         {
@@ -39,12 +40,25 @@ async function addComment(e) {
             return false;
         }
 
-        await setComment(tbody, );
+		Swal.fire({
+			icon: 'success',
+			title: '<h5 class="text-success">Success</h5>',
+			html: '<span class="text-success fw-semibold">Komentar anda berhasil di tambahkan !!!</span>',
+			timer: 1200
+		});
+
+		let data = j.data;
+        await setComment(tbody, data);
 
     }
     catch(err)
     {
-        console.error(err);
+        Swal.fire({
+			icon: 'error',
+			title: '<h5 class="text-danger">ERROR</h5>',
+			html: '<span class="text-danger fw-semibold">Komentar anda tidak dapat di masukan !!!</span>',
+			timer: 1200
+		});
     }   
 }
 
@@ -56,10 +70,10 @@ async function setComment(tbody, data=null) {
     let rateString = '';
 
     Array.from({ length: 5 }, (val, idx) => {
-        const startLength = parseInt(data.rating);
+        const startLength = parseInt(data.rate);
 
         if((idx + 1) <= startLength)
-            rateString += '<i class="fas fa-star text-primary"></i>';
+            rateString += '<i class="fas fa-star" style="color: #FFB900"></i>';
         else
             rateString += '<i class="fas fa-star text-secondary"></i>';
     });
@@ -68,14 +82,14 @@ async function setComment(tbody, data=null) {
                         '<div class="col-12">' +
                             '<h4 class="mb-2">JAMED</h4>' +
                             '<span>' + rateString + '</span>' +
-                            '<p class="my-0">'+data['text-review']+'</p>' +
+                            '<p class="my-0">'+data.comments+'</p>' +
                         '</div>' +
                     '</div>';
                     
     td.innerHTML = inner;
     rateString = null;
 
-    return row;
+    // return row;
 }
 
 
