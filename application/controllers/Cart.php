d80843937f0c7c5f41c3d5547f03876fbc15d1c4
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-12-28 09:24:01 +0700

    create function add course pada controller Cart

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -4,9 +4,38 @@ class Cart extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
+		$this->load->model(['course_model']);
     }
 
     public function index() {
-        echo $this->template->render('cart', [], 'course\\');
+		$email = $this->session->userdata('user')['email'];
+		$member = $this->db->where('email', $email)->get('members')->row_array();
+
+		$data['carts'] = $this->course_model->get_all_carts($member['id']);
+
+        echo $this->template->render('cart', $data, 'course\\');
     }
+
+	public function add(){
+		$post = $this->input->post();
+
+		$member = $this->db->where('email', $post['email'])->get('members')->row_array();
+
+		$data = [
+			'course_id' => $post['course_id'],
+			'member_id' => $member['id'],
+			'status' 	=> 'unpaid',
+		];
+
+		$insert = $this->db->insert('carts', $data);
+
+		header('Content-Type: application/json');
+		if($insert){
+			$res = ['success'=>true, 'message'=>'Data berhasil di simpan!'];
+			echo json_encode($res);
+		}else{
+			$res = ['success'=>false, 'message'=>'Data gagal di simpan!'];
+			echo json_encode($res);
+		}
+	}
 }
