7b52b110cf6f38c2041b1e9e5bd385e8c03ceef1
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-12-28 09:23:04 +0700

    create hitung ppn course and total price course

--- application/views/course/cart.php
+++ application/views/course/cart.php
@@ -10,6 +10,7 @@
 <div class="container">
         <div class="row">
             <div class="col-lg-8">
+				<?php foreach($carts as $value) : ?>
                 <!-- START CARD-->
                 <div class="panel shadow-sm bg-white text-dark mb-3">
                     <div class="panel-body p-2">
@@ -20,7 +21,7 @@
                             <div class="col-12 col-lg-8 position-relative top-0 left-0">
                                 <div class="row h-75">
                                     <div class="col-lg-9">
-                                        <h4 class="mb-1"><strong>MODEL JAMED</strong></h4>
+                                        <h4 class="mb-1"><strong><?=$value['course_title']?></strong></h4>
                                         <p class="text-secondary">8 lessons</p>
                                     </div>
                                     <div class="col-lg-3">
@@ -38,7 +39,7 @@
                                         <hr class="m-0 p-0 text-secondary">
                                         <div class="w-100 d-flex mt-1">
                                             <a href="javascript:void(0)" class="text-secondary text-capitalize mt-1">simpan ke <i>Wishlist</i> </a>
-                                            <h5 class="fw-semibold text-end py-1 ms-auto text-shadow">Rp. 100.000</h5>
+                                            <h5 class="fw-semibold text-end py-1 ms-auto text-shadow">Rp. <?=number_format($value['price'])?></h5>
                                         </div>
                                     </div>
                                 </div>
@@ -47,98 +48,27 @@
                     </div>
                 </div>
                 <!-- END CARD -->
-
-                <!-- START CARD-->
-                <div class="panel shadow-sm bg-white text-dark mb-3">
-                    <div class="panel-body p-2">
-                        <div class="row">
-                            <div class="col-12 col-lg-4">
-                                <img class="img-fluid" src="assets/images/sm2.jpg">
-                            </div>
-                            <div class="col-12 col-lg-8 position-relative top-0 left-0">
-                                <div class="row h-75">
-                                    <div class="col-lg-9">
-                                        <h4 class="mb-1"><strong>MODEL JAMED</strong></h4>
-                                        <p class="text-secondary">8 lessons</p>
-                                    </div>
-                                    <div class="col-lg-3">
-                                        <div class="d-flex justify-content-end">
-                                            <button type="button" class="btn btn-sm btn-primary shadow-sm ms-2 remove_cart">
-                                                <i class="fa-solid fa-trash text-white"></i>
-                                            </button>
-                                        </div>
-                                        
-                                    </div>
-                                    
-                                </div>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <hr class="m-0 p-0 text-secondary">
-                                        <div class="w-100 d-flex mt-1">
-                                            <a href="javascript:void(0)" class="text-secondary text-capitalize mt-1">simpan ke <i>Wishlist</i> </a>
-                                            <h5 class="fw-semibold text-end py-1 ms-auto text-shadow">Rp. 100.000</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- END CARD -->
-
-                <!-- START CARD -->
-                <div class="panel shadow-sm bg-white text-dark mb-3">
-                    <div class="panel-body p-2">
-                        <div class="row">
-                            <div class="col-12 col-lg-4">
-                                <img class="img-fluid" src="assets/images/sm2.jpg">
-                            </div>
-                            <div class="col-12 col-lg-8 position-relative top-0 left-0">
-                                <div class="row h-75">
-                                    <div class="col-lg-9">
-                                        <h4 class="mb-1"><strong>MODEL JAMED</strong></h4>
-                                        <p class="text-secondary">8 lessons</p>
-                                    </div>
-                                    <div class="col-lg-3">
-                                        <div class="d-flex justify-content-end">
-                                            <button type="button" class="btn btn-sm btn-primary shadow-sm ms-2 remove_cart">
-                                                <i class="fa-solid fa-trash text-white"></i>
-                                            </button>
-                                        </div>
-                                        
-                                    </div>
-                                    
-                                </div>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <hr class="m-0 p-0 text-secondary">
-                                        <div class="w-100 d-flex">
-                                            <a href="javascript:void(0)" class="text-secondary text-capitalize mt-1">simpan ke <i>Wishlist</i> </a>
-                                            <h5 class="fw-semibold text-end py-1 ms-auto text-shadow">Rp. 100.000</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- END CARD -->
+				<?php endforeach ?>
                 
             </div>
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-body">
+						<?php $course_price = array_sum(array_column($carts, 'price')); 
+							$ppn = $course_price * 11 / 100;
+						?>
+
                         <h5 class="fw-semibold text-uppercase">ringkasan</h5>
                         <hr class="border border-secondary mt-1"/>
                         <dl class="row">
                             <dd class="col-6 text-capitalize mb-0 fs-6">total harga</dd>
-                            <dt class="col-6 text-end fs-6">300.000</dt>
+                            <dt class="col-6 text-end fs-6">Rp <?=number_format($course_price) ?></dt>
                             <dd class="col-6 text-capitalize mb-0 fs-6">pajak</dd>
-                            <dt class="col-6 text-end fs-6">12.400</dt>
+                            <dt class="col-6 text-end fs-6">Rp <?=number_format($ppn) ?></dt>
                         </dl>
                         <hr class="text-secondary my-2" />
                         <div class="d-flex w-100">
-                            <h5 class="ms-auto fw-semibold">312.000</h5>
+                            <h5 class="ms-auto fw-semibold">Rp <?=number_format($course_price + $ppn) ?></h5>
                         </div>
                         <button type="button" class="btn btn-primary w-100 text-center text-capitalize text-white fw-semibold mt-3">
                             checkout
