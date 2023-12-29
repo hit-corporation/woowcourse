52ac8614977c37961a649a894baff1ef13923acc
Ahmad Fauzi <fauzi.enginer@gmail.com>
2023-12-29 10:28:57 +0700

    fixing query detail di model instructor

--- application/models/Instructor_model.php
+++ application/models/Instructor_model.php
@@ -10,9 +10,9 @@ class Instructor_model extends CI_Model {
 	}
 
 	public function detail($id){
-		$this->db->select('i.*, m.photo');
+		$this->db->select('m.*, m.photo');
 		$this->db->from('instructors i');
-		$this->db->where('i.id', $id);
+		$this->db->where('m.id', $id);
 		$this->db->join('members m', 'm.email = i.email');
 		return $this->db->get()->row_array();
 	}
