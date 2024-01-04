d244bca1f271b44884565cf3dfd7e77864a1832f
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-01-04 10:35:32 +0700

    delete acordion 2 and 3 pada UI detail course

--- application/views/course/detail.php
+++ application/views/course/detail.php
@@ -65,13 +65,7 @@ $this->layout('layouts::main_template', ['title' => 'Course']) ?>
 						<p class="text-justify mt-2">
 							<?=$data['description']?>
 						</p>
-						<!-- <h4 class="text-uppercase mb-1">what will you learn</h4>
-						<div class="separator mb-3"></div>
-						<ul>
-							<li>Lorem ipsum dolor sit amet.</li>
-							<li>consectetur adipiscing elit.</li>
-							<li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-						</ul> -->
+						
 					</div>
 					<!-- TAB 2 -->
 					<div class="tab-pane fade" id="topic" role="tabpanel" tabindex="0">
@@ -80,7 +74,7 @@ $this->layout('layouts::main_template', ['title' => 'Course']) ?>
 							<div class="accordion-item">
 								<h2 class="accordion-header">
 									<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-										Accordion Item #1
+										Course Videos
 									</button>
 								</h2>
 								<div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -88,30 +82,6 @@ $this->layout('layouts::main_template', ['title' => 'Course']) ?>
 										<?php foreach ($videos as $key => $value) : ?>
 											<video width="500" src="<?=!empty($value['video']) ? base_url('assets/files/upload/courses/').$value['video'] : '' ?>" controls></video>
 										<?php endforeach ?>
-									</div>
-								</div>
-							</div>
-							<div class="accordion-item">
-								<h2 class="accordion-header">
-									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-										Accordion Item #2
-									</button>
-								</h2>
-								<div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-									<div class="accordion-body">
-										<strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-									</div>
-								</div>
-							</div>
-							<div class="accordion-item">
-								<h2 class="accordion-header">
-									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-										Accordion Item #3
-									</button>
-								</h2>
-								<div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-									<div class="accordion-body">
-										<strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
 									</div>
 								</div>
 							</div>
