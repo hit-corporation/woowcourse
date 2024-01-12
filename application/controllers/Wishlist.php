b6ceba1b7af7a55be50e96f511f94130b69760f8
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-01-12 09:17:05 +0700

    create proses delete data wishlist di controller Wishlist

--- application/controllers/Wishlist.php
+++ application/controllers/Wishlist.php
@@ -12,9 +12,24 @@ class Wishlist extends MY_Controller {
 		$email = $this->session->userdata('user')['email'];
 		$member = $this->db->where('email', $email)->get('members')->row_array();
 
-		$data['carts'] = $this->course_model->get_all_wishlist($member['id']);
+		$data['wishlists'] = $this->course_model->get_all_wishlist($member['id']);
 
         echo $this->template->render('wishlist', $data, 'course\\');
     }
 
+	public function delete(){
+		$post = $this->input->post();
+		$id = $post['id'];
+
+		$q = $this->db->where('id', $id)->delete('wishlists');
+		
+		if($q){
+			$res = ['success' => true, 'message' => 'Data berhasil di hapus!'];
+		} else {
+			$res = ['success' => false, 'message' => 'Data gagal di hapus!'];
+		}
+
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($res);
+	}
 }
