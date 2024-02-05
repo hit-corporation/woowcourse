ac863dcf64b76586368d7870dc00208117484506
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-02-05 13:51:56 +0700

    ubah text UI email sent

--- application/views/login/email_sent.php
+++ application/views/login/email_sent.php
@@ -5,17 +5,19 @@
 <?php $this->end() ?>
 
 <?php $this->start('body') ?>
+
+<div class="sent-email-bg"></div>
+
 <div class="container">
     <div class="row">
         <div class="col-12">
             <p class="fs-5 text-shadow">
-                An email to <strong></strong> has been sent with instructions to activate your account. 
-                The activation link is only valid for 1 hours. If you don't receive the instructions try checking your junk or spam filters. 
+				Email berhasil di kirim ke alamat email user. silahkan masuk ke kontak masuk email user dan tekan tombol aktivasi. 
             </p>
             <div class="d-flex flex-nowrap mt-3 align-items-center">
                 <a class="btn btn-sm btn-primary text-white fs-6" href="<?=base_url()?>">Homepage</a>
-                <span class="fs-6 mx-2">Or</span>
-                <a href="javascript:void(0)" class="fs-6">Resend activation mail</a>
+                <!-- <span class="fs-6 mx-2">Or</span> -->
+                <!-- <a href="javascript:void(0)" class="fs-6">Resend activation mail</a> -->
             </div>
         </div>
     </div>
