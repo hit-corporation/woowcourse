ba03fa37ed2a21deb916e13ca06d0dfed613d49e
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-02-20 11:11:38 +0700

    change modal pilih pembayaran pada UI cart

--- application/views/course/cart.php
+++ application/views/course/cart.php
@@ -92,12 +92,15 @@
 			<h5 class="modal-title">Pilih Pembayaran</h5>
 			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 		</div>
+
 		<div class="modal-body">
-			<p>Modal body text goes here.</p>
+			<p class="fw-bold" style="cursor: pointer;" data-bs-dismiss="modal">Transfer bank BCA ></p>
+			<p class="fw-bold" style="cursor: pointer;" data-bs-dismiss="modal">Transfer bank Mandiri ></p>
+			<p class="fw-bold" style="cursor: pointer;" data-bs-dismiss="modal">Transfer bank BNI ></p>
 		</div>
+
 		<div class="modal-footer">
 			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-			<button type="button" class="btn btn-primary">Save changes</button>
 		</div>
 		</div>
 	</div>
