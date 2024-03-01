6f523495ddfb0b7b745f684ade78d48e1f51660a
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-03-01 14:55:53 +0700

    create join to tabel instructor and members pada function index di controller History

--- application/controllers/History.php
+++ application/controllers/History.php
@@ -13,7 +13,9 @@ class History extends MY_Controller {
 
 		$this->db->where('status', 'paid')
 			->where('ca.member_id', $memberId)
-			->join('courses c', 'c.id = ca.course_id', 'left');
+			->join('courses c', 'c.id = ca.course_id', 'left')
+			->join('instructors i', 'i.id = c.instructor_id')
+			->join('members m', 'm.email = i.email');
 		$data = $this->db->get('carts ca')->result_array(); 
 
 		echo $this->template->render('index', ['data' => $data]);
